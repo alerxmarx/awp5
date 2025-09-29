45104cc09cc76bbb808cb959b464b6d5579b7ac2
amartinezo <amartinezo@outlook.com>
2025-09-29 17:43:12 -0600

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>awp5</h1>
+      <h1>awp5 Alejandro Mtz</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
